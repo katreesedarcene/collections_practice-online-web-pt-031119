12ce41b7e0d1c6fb607881edf680dd2f4b5f0a21
Kat B <katreesebarnes@gmail.com>
2019-03-20 13:02:18 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -64,7 +64,7 @@ def sort_array_char_count(array)
    
    def add_s(array)
     array.each_with_index.collect do   |element, index| 
-      if index != index[-2]
+      if index[1, 3, 4]
         "#{element}s"
       end
     end
