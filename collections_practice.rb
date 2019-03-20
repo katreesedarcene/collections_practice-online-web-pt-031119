3836b532861e30a3a10112eb7cace4bb457b185c
Kat B <katreesebarnes@gmail.com>
2019-03-20 10:48:56 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -64,8 +64,8 @@ def sort_array_char_count(array)
    end
    def add_s(array)
      array.each_with_index.collect {|element, index|}
-     binding.pry
+     
       
          
-     end
+    
    end
